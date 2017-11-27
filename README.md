56ca1892d40ea65615367c51aabcf6d54c67d2a2
Mariusz Stróż <manieczny86@gmail.com>
2017-11-27 10:37:06 +0100

    Salesmanago component ver 0.0.1

---
+++ README.md
@@ -0,0 +1,64 @@
+# yii2-salesmanago
+Salesmanago component for Yii 2 framework
+
+[SALESmanago Technical API Documentation](http://support.salesmanago.pl/wp-content/uploads/2014/10/SALESmanago-API-en.pdf)
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Run
+
+```
+composer require --prefer-source "mrstroz/yii2-salesmanago" "*"
+```
+
+or add
+
+```
+"mrstroz/yii2-salesmanago": "*"
+```
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+1. Add component to your config file
+```php
+'components' => [
+    // ...
+    'salesmanago' => [
+        'class' => 'mrstroz\salesmanago\Salesmanago',
+        'clientId' => 'xxxxxx',
+        'apiKey' => 'xxxxxx',
+        'apiSecret' => 'xxxxxx',
+    ],
+]
+```
+
+2. Add new contact to SALESmanago
+```php
+
+$salesmanago = Yii::$app->salesmanago;
+    $result = $salesmanago->contactUpsert([
+        'contact' => [
+            'email' => 'example@example.com',
+            'state' => 'CUSTOMER',
+        ],
+        'owner' => 'owner@owner.com',
+        'tags' => [
+            'TAG'
+        ],
+        'properties' => array('page' => 'newsletter'),
+        'lang' => 'PL',
+        'useApiDoubleOptIn' => true,
+        'forceOptIn' => true,
+        'forceOptOut' => false,
+        'forcePhoneOptIn' => true,
+        'forcePhoneOptOut' => false
+    ]);
+```
+
+Check [SALESmanago Technical API Documentation](http://support.salesmanago.pl/wp-content/uploads/2014/10/SALESmanago-API-en.pdf) for all available options.
---
+++ Salesmanago.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace mrstroz\salesmanago;
+
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * Class Salesmanago
+ * Salesmanago component
+ * @package mrstroz\salesmanago
+ *
+ * @author Mariusz Stróż <info@inwave.pl>
+ */
+class Salesmanago extends Component
+{
+
+    /**
+     * @var string Can be found under „Integration” menu of „Settings” section
+     */
+    public $clientId;
+
+    /**
+     * @var string Random pice of string
+     */
+    public $apiKey;
+
+    /**
+     * @var string Can be found under „Integration” section in application menu
+     */
+    public $apiSecret;
+
+    /**
+     * @var string
+     */
+    public $endpoint = 'app2.salesmanago.pl';
+
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        if (!$this->clientId) {
+            throw new InvalidConfigException('$clientId not set');
+        }
+
+        if (!$this->apiKey) {
+            throw new InvalidConfigException('$apiKey not set');
+        }
+
+        if (!$this->apiSecret) {
+            throw new InvalidConfigException('$apiSecret not set');
+        }
+
+        if (!$this->endpoint) {
+            throw new InvalidConfigException('$endpoint not set');
+        }
+
+        parent::init();
+    }
+
+    /**
+     * Adding contact to Salesmanago
+     * @param array $data
+     * @return \stdClass Salesmanago response
+     */
+    public function contactUpsert($data)
+    {
+        $data = array_merge(
+            array(
+                'clientId' => $this->clientId,
+                'apiKey' => $this->apiKey,
+                'requestTime' => time(),
+                'sha' => sha1($this->apiKey . $this->clientId . $this->apiSecret),
+            ),
+            $data
+        );
+
+        $json = json_encode($data);
+        $result = $this->doRequest('http://' . $this->endpoint . '/api/contact/upsert', $json);
+        return json_decode($result);
+    }
+
+    /**
+     * Do request by CURL
+     * @param $url
+     * @param $data
+     * @return mixed
+     */
+    private function doRequest($url, $data)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data)
+            )
+        );
+
+        return curl_exec($ch);
+    }
+
+
+}
